133d5ea3f39fcc58ac021548edbc96568f5eb533
AndreiAgmata <agmataa1124@gmail.com>
2023-12-15 17:12:11 -0500

    Added basic animation

--- src/app/Hero.js
+++ src/app/Hero.js
@@ -4,20 +4,59 @@ import memoji from "../../public/memoji.png";
 import Image from "next/image";
 import { Button } from "react-bootstrap";
 
+import { TimelineLite, Power3 } from "gsap";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
 function Hero() {
+  let header = useRef();
+  let emoji = useRef();
+  const tl = new gsap.timeline();
+
+  useEffect(() => {
+    let headerFirst = header.children[0].children[0];
+    let headerSecond = headerFirst.nextSibling;
+    let headerThird = headerSecond.nextSibling;
+
+    let emojiImage = emoji;
+
+    let ctx = gsap.context(() => {
+      tl.staggerFrom(
+        [headerFirst.children, headerSecond.children, headerThird.children],
+        1,
+        {
+          opacity: 0,
+          ease: Power3.easeOut,
+          delay: 0.8,
+        },
+        0.15
+      ).from(emojiImage, 1.3, { y: 500, ease: Power3.easeOut });
+    });
+
+    return () => ctx.revert();
+  }, [tl]);
+
   return (
     <div className={app.home} id="home">
       <div className={app.left}>
-        <section>
-          <h1 className={variables.title_stroke} id="top">
-            Hello 👋🏻, I'm
-          </h1>
-          <h1 className={variables.title_solid} id="mid">
-            Andrei Agmata
-          </h1>
-          <h1 className={variables.title_stroke} id="bot">
-            Web Developer
-          </h1>
+        <section ref={(el) => (header = el)}>
+          <div>
+            <div>
+              <h1 className={variables.title_stroke} id="top">
+                Hello 👋🏻, I'm
+              </h1>
+            </div>
+            <div>
+              <h1 className={variables.title_solid} id="mid">
+                Andrei Agmata
+              </h1>
+            </div>
+            <div>
+              <h1 className={variables.title_stroke} id="bot">
+                Web Developer
+              </h1>
+            </div>
+          </div>
           <p>
             I create immersive digital experiences <br />
             that go beyond mere websites, <br /> aiming for innovation and
@@ -44,7 +83,11 @@ function Hero() {
       <div className={app.right}>
         <div className={app.bg}>
           <section className={app.overlay_container}>
-            <img src="/memoji.png" alt="memoji"></img>
+            <img
+              src="/memoji.png"
+              alt="memoji"
+              ref={(el) => (emoji = el)}
+            ></img>
           </section>
         </div>
       </div>

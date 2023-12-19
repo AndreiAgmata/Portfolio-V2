08121ed872d29a82927ac9977ef3131cdde785f5
AndreiAgmata <agmataa1124@gmail.com>
2023-12-19 17:05:36 -0500

    Added scrolltrigger

--- src/app/Projects.js
+++ src/app/Projects.js
@@ -8,15 +8,14 @@ import projectsData from "../../public/projects.json";
 import gsap from "gsap";
 import { Power3 } from "gsap";
 import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerEase(Power3);
 
 function Projects() {
-  // let project = useRef([]);
-  // project.current = [];
-
   let projectRefs = useRef([]);
   projectRefs.current = [];
-
-  const tl = new gsap.timeline();
 
   const addToRefs = (el) => {
     if (el && !projectRefs.current.includes(el)) {
@@ -27,120 +26,83 @@ function Projects() {
   useEffect(() => {
     let ctx = gsap.context(() => {
       projectRefs.current.forEach((projectRef) => {
-        tl.from(
-          projectRef.children[1].children[0],
-          1.2,
-          { xPercent: -100, opacity: 0, ease: Power3.easeOut },
-          "Start"
-        );
-
-        tl.from(
-          projectRef.children[0].children[1].children[0].children[0],
-          1.2,
-          {
-            yPercent: 100,
-            opacity: 0,
-            ease: Power3.easeOut,
+        const tl = new gsap.timeline({
+          scrollTrigger: {
+            trigger: projectRef,
+            start: "top center+=100",
+            toggleActions: "play none none reverse",
           },
-          0.15
-        )
+        });
+
+        let q = gsap.utils.selector(projectRef);
+
+        tl.from(q("#project-num"), {
+          xPercent: -100,
+          opacity: 0,
+          ease: "sine.inOut",
+          duration: 0.5,
+        })
           .from(
-            projectRef.children[0].children[0].children[0].children[0],
-            1.2,
-            {
-              xPercent: -100,
-              opacity: 0,
-              ease: Power3.easeOut,
-            },
-            0.15
-          )
-          .from(
-            projectRef.children[0].children[1].children[1].children[0],
-            1.2,
+            q("#title"),
             {
               yPercent: 100,
               opacity: 0,
               ease: Power3.easeOut,
+              duration: 0.7,
             },
-            0.15
+            0.2
           )
           .from(
-            projectRef.children[0].children[1].children[2].children[0],
-            1.2,
-            {
-              opacity: 0,
-              ease: Power3.easeOut,
-              delay: 0.8,
-            },
-            0.15
-          )
-          .from(
-            projectRef.children[0].children[1].children[2].children[1],
-            1.2,
+            q("#image"),
             {
               scale: 0,
               opacity: 0,
               ease: Power3.easeOut,
             },
-            1.2
+            0.2
           )
           .from(
-            projectRef.children[0].children[1].children[2].children[2]
-              .children[1],
-            1.2,
-            {
-              xPercent: -100,
-              opacity: 0,
-              ease: Power3.easeOut,
-              delay: 1,
-            },
-            0.15
-          )
-          .from(
-            projectRef.children[0].children[0].children[1],
-            3.6,
+            q("#vLine"),
             {
               height: 0,
               opacity: 0,
               ease: Power3.easeOut,
+              duration: 1,
             },
-            0.15
+            0.2
           )
           .from(
-            projectRef.children[0].children[1].children[2].children[2]
-              .children[0],
-            1.2,
+            q("#sub"),
             {
-              width: 0,
+              xPercent: -100,
+              opacity: 0,
+              ease: Power3.easeOut,
+            },
+            0.2
+          )
+          .from(
+            q("#details"),
+            {
               opacity: 0,
               ease: Power3.easeOut,
             },
             0.5
           )
+          .from(q("#hLine1"), {
+            width: 0,
+            ease: Power3.easeOut,
+          })
+          .from(q("#role"), {
+            xPercent: -100,
+            ease: Power3.easeOut,
+          })
           .from(
-            projectRef.children[0].children[1].children[2].children[2]
-              .children[2],
-            1.2,
+            q("#hLine2"),
             {
               width: 0,
-              opacity: 0,
               ease: Power3.easeOut,
             },
-            0.7
-          )
-          .staggerFrom(
-            [
-              projectRef.children[0].children[1].children[2].children[3]
-                .children[0],
-              projectRef.children[0].children[1].children[2].children[3]
-                .children[1],
-            ],
-            0.7,
-            {
-              opacity: 0,
-              ease: Power3.easeOut,
-            },
-            0.2
+            1
           );
       });
     });
@@ -163,29 +125,29 @@ function Projects() {
             <div className={projects.projectInfo}>
               <div className={projects.sideBar}>
                 <div style={{ overflow: "hidden" }}>
-                  <h2>{project.id}</h2>
+                  <h2 id="project-num">{project.id}</h2>
                 </div>
-                <div className={projects.verticalline}></div>
+                <div className={projects.verticalline} id="vLine"></div>
               </div>
               <div className={projects.content}>
                 <div className={projects.titleWrapper}>
-                  <h1>{project.title}</h1>
+                  <h1 id="title">{project.title}</h1>
                 </div>
                 <div className={projects.subtitleWrapper}>
-                  <h4>{project.sub}</h4>
+                  <h4 id="sub">{project.sub}</h4>
                 </div>
                 <div className={projects.details}>
-                  <p>{project.details}</p>
+                  <p id="details">{project.details}</p>
                   <div className={projects.mobileImg}>
                     <img src={project.image} alt="Project Image"></img>
                     <div className={projects.overlay}></div>
                   </div>
                   <div className={projects.role} style={{ overflow: "hidden" }}>
-                    <div className={projects.horizontalline}></div>
-                    <p>
+                    <div className={projects.horizontalline} id="hLine1"></div>
+                    <p id="role">
                       <strong>Role :</strong> {project.role}
                     </p>
-                    <div className={projects.horizontalline}></div>
+                    <div className={projects.horizontalline} id="hLine2"></div>
                   </div>
                   <div className={projects.buttons}>
                     <Button
@@ -210,7 +172,7 @@ function Projects() {
               style={{ overflow: "hidden" }}
             >
               <div className={projects.imageWrapper}>
-                <img alt="Project Image" src={project.image}></img>
+                <img alt="Project Image" src={project.image} id="image"></img>
               </div>
             </div>
           </div>

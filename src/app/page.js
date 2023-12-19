d19c7c8269733561a6b96fe7ac79fd166e58e7f8
AndreiAgmata <agmataa1124@gmail.com>
2023-12-19 04:04:37 -0500

    Added  back preloader

--- src/app/page.js
+++ src/app/page.js
@@ -20,22 +20,22 @@ export default function Home() {
   let loading = useRef();
   const tl = new gsap.timeline();
 
-  // useEffect(() => {
-  //   let ctx = gsap.context(() => {
-  //     tl.to(loading, {
-  //       opacity: 0,
-  //       ease: Power3.easeOut,
-  //       duration: 0.7,
-  //       delay: 2.8,
-  //     });
-  //   });
-  //   return () => ctx.revert();
-  // });
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      tl.to(loading, {
+        opacity: 0,
+        ease: Power3.easeOut,
+        duration: 0.7,
+        delay: 2.8,
+      });
+    });
+    return () => ctx.revert();
+  });
   return (
     <>
-      {/* <div ref={(el) => (loading = el)}>
+      <div ref={(el) => (loading = el)}>
         <Loading />
-      </div> */}
+      </div>
       <SideBar />
       <Hero />
       <Projects />
